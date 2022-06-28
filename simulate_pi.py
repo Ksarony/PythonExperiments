bb1bc6b77b529a0630e5bf451c10f23a221f8c7a
hasinfarhan <opangkteyo@gmail.com>
2022-06-28 23:13:57 +0200

    monte carlo simulation of pi

---
+++ simulate_pi.py
@@ -0,0 +1,15 @@
+import random
+
+
+def monte_carlo_pi(num_it):
+    count = 0
+    for i in range(0, num_it):
+        x = random.uniform(-1.0, 1)
+        y = random.uniform(-1.0, 1)
+        if x**2 + y**2 < 1:
+            count += 1
+    pi = (count / num_it) * 4
+    return pi
+
+
+print(monte_carlo_pi(10000))
